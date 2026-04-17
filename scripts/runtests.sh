0df4dbb6e31812c1562fb25f120db4d86d5e6b95
Skylar Abruzese <skylar@abruzese.com>
2026-04-16 20:36:19 -0400

    update test scripts

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,5 +2,7 @@
 
 SHARED_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
+TESTS="shared/${1:-tests}"
+
 # run tests
-"$SHARED_PATH/scripts/drun.sh" ./eth/eth --compilerpath shared/scripts --testpath ./eth/tests/pa1 ./shared/scripts/ethScript
+"$SHARED_PATH/scripts/drun.sh" ./eth/eth --compilerpath shared/scripts --testpath "$TESTS" ./shared/scripts/ethScript
